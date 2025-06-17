865124401b43a3f03b9c7d5b74ad6db61f669e3b
zhongzhounb <792899501@qq.com>
2025-06-17 11:53:23 +0800

    总结了主动技能效果一览，方便之后设置前后端交互

--- src/gamecommand.h
+++ src/gamecommand.h
@@ -20,13 +20,17 @@ public:
     QVariant getUserChoice() const { return m_userChoice; }
 
     // 检查是否需要用户交互（默认不需要交互）
-    virtual bool requiresUserInput() const { return false; }
+    bool requiresUserInput() const { return false; }
 
-    // 【真人：前端→后端/AI：后端】设置选项 (由 GameController 或 AI 调用)
-    void setChoice(const QVariant& choice) { m_userChoice = choice; }
+    // 获得选项
+    virtual QVariantMap getPromptData() const { return QVariantMap(); };
 
-    // 【真人：后端→前端】向 UI 发送请求信号。(在回放或AI玩家时不调用）
-    virtual void prompt(GameController* controller) { Q_UNUSED(controller); }
+    // 获取默认选项
+    virtual QVariantMap getAutoChoice(const QVariantMap& promptData);
+
+    // 设置选项
+    void setChoice(const QVariantMap& choice) { m_userChoice = choice; }
+
 
     // 执行命令的核心逻辑。此方法假定 m_userChoice 已经设置。
     virtual void execute(GameState* state, GameController* controller) = 0;
@@ -39,7 +43,7 @@ protected:
     CommandType m_type;
     int m_priority;
     Player* m_sourcePlayer; // 触发此命令的玩家
-    QVariant m_userChoice; // 存储用户/AI的选择，用于回放
+    QVariantMap m_userChoice; // 存储用户/AI的选择，用于回放
     Card* m_card;//【卡牌命令】
     bool m_isFailed;//【卡牌命令】是否激活后无效
     Player* m_activePlayer;//【卡牌命令】激活者（大部分不用传入）
